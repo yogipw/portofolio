7b923d8761ba6850fe7821825bf0f673741f12ba
yogipw <yogigontor12@gmail.com>
2023-08-17 20:47:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                base: "#1B1A17",
+                gold: "#F0A500"
+            }
+        },
+    },
+    plugins: [],
+}
